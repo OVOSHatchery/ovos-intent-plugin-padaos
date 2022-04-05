781ffdfed159383273ce3028d74ab619aff14cc5
jarbasai <jarbasai@mailfence.com>
2022-04-05 23:19:09 +0100

    add lang to intent api

--- ovos_intent_plugin_padaos/__init__.py
+++ ovos_intent_plugin_padaos/__init__.py
@@ -12,25 +12,22 @@ class PadaosExtractor(IntentExtractor):
                          priority=priority, segmenter=segmenter)
         self.container = IntentContainer()
 
-    def detach_intent(self, intent_name):
-        super().detach_intent(intent_name)
-
-    def register_entity(self, entity_name, samples=None):
+    def register_entity(self, entity_name, samples=None, lang=None):
         samples = samples or [entity_name]
         super().register_entity(entity_name, samples)
         self.container.add_entity(entity_name, samples)
 
-    def register_intent(self, intent_name, samples=None):
+    def register_intent(self, intent_name, samples=None, lang=None):
         samples = samples or [intent_name]
         super().register_intent(intent_name, samples)
         self.container.add_intent(intent_name, samples)
 
-    def calc_intent(self, utterance, min_conf=0.0):
+    def calc_intent(self, utterance, min_conf=0.5, lang=None):
         utterance = utterance.strip().lower()
         intent = self.container.calc_intent(utterance)
         if intent["name"]:
             remainder = self.get_utterance_remainder(
-                utterance, samples=self._intent_samples[intent["name"]])
+                utterance, samples=self.registered_intents[intent["name"]])
             intent["intent_engine"] = "padaos"
             intent["intent_type"] = intent.pop("name")
             intent["utterance"] = utterance
@@ -38,9 +35,4 @@ class PadaosExtractor(IntentExtractor):
             modifier = len(self.segmenter.segment(utterance))
             intent["conf"] = 1 / modifier - 0.1
             return intent
-        return {'conf': 0,
-                'intent_type': 'unknown',
-                'entities': {},
-                'utterance': utterance,
-                'utterance_remainder': utterance,
-                'intent_engine': 'padaos'}
+        return None
