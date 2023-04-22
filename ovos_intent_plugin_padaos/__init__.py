5b5d1fc6d82722a119c004ccaf919ef9859e7dd1
JarbasAi <jarbasai@mailfence.com>
2023-04-22 07:52:57 +0100

    multilingual_support

--- ovos_intent_plugin_padaos/__init__.py
+++ ovos_intent_plugin_padaos/__init__.py
@@ -10,21 +10,33 @@ class PadaosExtractor(IntentExtractor):
                  segmenter=None):
         super().__init__(config, strategy=strategy,
                          priority=priority, segmenter=segmenter)
-        self.container = IntentContainer()
+        self.engines = {}  # lang: IntentContainer
+
+    def _get_engine(self, lang=None):
+        lang = lang or self.lang
+        if lang not in self.engines:
+            self.engines[lang] = IntentContainer()
+        return self.engines[lang]
 
     def register_entity(self, entity_name, samples=None, lang=None):
+        lang = lang or self.lang
+        container = self._get_engine(lang)
         samples = samples or [entity_name]
         super().register_entity(entity_name, samples)
-        self.container.add_entity(entity_name, samples)
+        container.add_entity(entity_name, samples)
 
     def register_intent(self, intent_name, samples=None, lang=None):
+        lang = lang or self.lang
+        container = self._get_engine(lang)
         samples = samples or [intent_name]
         super().register_intent(intent_name, samples)
-        self.container.add_intent(intent_name, samples)
+        container.add_intent(intent_name, samples)
 
-    def calc_intent(self, utterance, min_conf=0.5, lang=None):
+    def calc_intent(self, utterance, min_conf=0.5, lang=None, session=None):
+        lang = lang or self.lang
+        container = self._get_engine(lang)
         utterance = utterance.strip().lower()
-        intent = self.container.calc_intent(utterance)
+        intent = container.calc_intent(utterance)
         if intent["name"]:
             remainder = self.get_utterance_remainder(
                 utterance, samples=self.registered_intents[intent["name"]])
